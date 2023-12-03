ca431df2c4d02e742301df7607babe6591cfb7e5
Seynox <seynoxthegentleman@gmail.com>
2023-12-03 20:12:48 +0100

    Refactored velocity in entity to stay consistent

--- entities/entity.gd
+++ entities/entity.gd
@@ -4,12 +4,12 @@ class_name Entity extends CharacterBody3D
 @export var HEALTH: float = 1.0
 @export var SPEED: float = 10 # In meters per second
 @export var ATTACK_DISTANCE: float = 1.5
-@export var DAMAGES_ON_HIT: float = 1.0
+@export var ATTACK_DAMAGE: float = 1.0
 @export var CAN_BE_HIT: bool = true
 @export var GRAVITY_MULTIPLIER: float = 1.0
 
 var GRAVITY: float = ProjectSettings.get_setting("physics/3d/default_gravity")
-var gravity_velocity: Vector3 = Vector3.ZERO
+var gravity_velocity: Vector3
 
 # Movements
 
@@ -17,22 +17,16 @@ func _physics_process(delta: float):
 	if global_position.y <= -10:
 		_die()
 	
-	_apply_gravity(delta)
+	gravity_velocity = _calculate_gravity_velocity(delta)
+	velocity += gravity_velocity
 	move_and_slide()
 
-func _apply_gravity(delta: float) -> void:
+func _calculate_gravity_velocity(delta: float) -> Vector3:
 	var gravity_force = GRAVITY * GRAVITY_MULTIPLIER
 	if is_on_floor() and gravity_force >= 0:
-		gravity_velocity = Vector3.ZERO
-	else:
-		gravity_velocity = gravity_velocity.move_toward(Vector3(0, velocity.y - gravity_force, 0), gravity_force * delta)
-		
-	velocity += gravity_velocity
-
-func walk_towards(pos: Vector3) -> void:
-	var direction = global_position.direction_to(pos).normalized()
-	velocity.x = direction.x * SPEED
-	velocity.z = direction.z * SPEED
+		return Vector3.ZERO
+	
+	return gravity_velocity.move_toward(Vector3(0, velocity.y - gravity_force, 0), gravity_force * delta)
 
 # Health
 
@@ -55,4 +49,4 @@ func _die() -> void:
 
 func take_hit_from(source: Entity, multiplicator: float = 1) -> void:
 	if CAN_BE_HIT:
-		reduce_health(source.DAMAGES_ON_HIT * multiplicator)
+		reduce_health(source.ATTACK_DAMAGE * multiplicator)
