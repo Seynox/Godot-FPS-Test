fb448f19e4e1624b835baf2ef954fef972d0ffb8
Seynox <seynoxthegentleman@gmail.com>
2023-12-10 23:35:42 +0100

    Moved random ability getter to ability registry, added level filter

--- autoloaded/ability_registry.gd
+++ autoloaded/ability_registry.gd
@@ -28,3 +28,25 @@ func _append_ability(rarity: Rarity.Level, ability: PackedScene):
 	var abilities_list: Array = ABILITIES.get(rarity, [])
 	abilities_list.append(ability)
 	ABILITIES[rarity] = abilities_list
+
+func get_random_ability(rarity: Rarity.Level, maximum_level: int = 0) -> Ability:
+	var abilities: Array = AbilityRegistry.ABILITIES.get(rarity, [])
+	abilities = abilities.filter(
+		func(ability):
+			return _can_ability_be_generated(ability, maximum_level)
+	)
+	
+	if abilities.is_empty():
+		return null
+	
+	var possibilities: int = abilities.size() - 1
+	var random_index: int = randi_range(0, possibilities)
+	
+	var ability_scene: PackedScene = abilities[random_index]
+	return ability_scene.instantiate()
+
+func _can_ability_be_generated(scene: PackedScene, maximum_level: int) -> bool:
+	var ability: Ability = scene.instantiate()
+	var is_at_valid_level: bool = maximum_level >= ability.GENERATE_SINCE_LEVEL
+	ability.queue_free()
+	return is_at_valid_level
