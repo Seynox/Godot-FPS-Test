407f2c81ba55b08a250b67df2de7a45f59aeda82
Seynox <seynoxthegentleman@gmail.com>
2023-12-11 00:06:42 +0100

    Made Hitscan weapons able to damage Breakables (Needs refactor)

--- abilities/weapons/hitscan_weapon.gd
+++ abilities/weapons/hitscan_weapon.gd
@@ -3,11 +3,11 @@ class_name HitscanWeapon extends CooldownWeapon
 
 func _attack(player: Player, _delta: float):
 	attack_started.emit()
-	var aimed_entity: Entity = _get_aimed_entity(player.camera)
-	if aimed_entity != null:
-		_apply_damages(player, aimed_entity)
+	var aimed_object: Node3D = _get_aimed_object(player.camera)
+	if aimed_object != null:
+		_apply_damages(player, aimed_object)
 
-func _get_aimed_entity(camera: Camera3D) -> Entity:
+func _get_aimed_object(camera: Camera3D) -> Node3D:
 	var mouse_position = get_viewport().get_mouse_position() # TODO Check if it works in multiplayer
 	var ray_origin = camera.project_ray_origin(mouse_position)
 	var ray_max = ray_origin + camera.project_ray_normal(mouse_position) * self.ATTACK_RANGE
@@ -18,12 +18,11 @@ func _get_aimed_entity(camera: Camera3D) -> Entity:
 	
 	var space = camera.get_world_3d().direct_space_state
 	var result = space.intersect_ray(ray)
-	var collidedNode = result.get("collider")
-	
-	if collidedNode != null && collidedNode is Entity:
-		return collidedNode
-	return null
+	return result.get("collider")
 
-func _apply_damages(player: Player, entity_attacked: Entity):
-	attacked.emit(entity_attacked)
-	entity_attacked.take_hit_from(player, ATTACK_DAMAGE)
+func _apply_damages(player: Player, object_attacked: Node3D): # TODO Make a damageable class? Refactor to weapon interface
+	if object_attacked is Entity:
+		attacked.emit(object_attacked)
+		object_attacked.take_hit_from(player, ATTACK_DAMAGE)
+	elif object_attacked is BreakableInteractible:
+		object_attacked.try_hitting(player)
