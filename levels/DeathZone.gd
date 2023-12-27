45e4dd3689bb12c0117988a7c670f6bfa7a6d9ba
Seynox <seynoxthegentleman@gmail.com>
2023-12-27 12:49:40 +0100

    Changed temporary revolver model

--- levels/DeathZone.gd
+++
@@ -1,6 +0,0 @@
-extends Area3D
-
-func _on_body_entered(body: Node3D):
-	if is_multiplayer_authority() and body is Entity:
-		var damages: float = body.MAX_HEALTH / 2
-		body.reduce_health.rpc(damages)
