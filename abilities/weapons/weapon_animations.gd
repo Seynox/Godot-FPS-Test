12db03ab46bbc330ad761af8e401985340b5e1b4
Seynox <seynoxthegentleman@gmail.com>
2023-12-27 17:58:24 +0100

    Added revolver reset animation, fixed animation canceling

--- abilities/weapons/weapon_animations.gd
+++ abilities/weapons/weapon_animations.gd
@@ -5,8 +5,10 @@ extends AnimationPlayer
 @export var RELOADING_ANIMATION_NAME: StringName = "Reloading"
 
 func _on_attack():
+	stop()
 	play(ATTACK_ANIMATION_NAME)
 
 
 func _on_reload():
+	stop()
 	play(RELOADING_ANIMATION_NAME)
