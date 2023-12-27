a2a675bba3bfd2b9cc1ffa2931e18c5755cbac55
Seynox <seynoxthegentleman@gmail.com>
2023-12-27 18:31:23 +0100

    Added floor friction to impulse dash

--- abilities/dashes/impulse_dash.gd
+++ abilities/dashes/impulse_dash.gd
@@ -1,6 +1,8 @@
 class_name ImpulseDash extends CooldownDash
 # Gives a speed impulse to the player
 
+@export var FRICTION_FORCE: float
+
 var dash_velocity: Vector3
 var can_dash: bool = true # Disallow dashing mutliple times in the air
 
@@ -18,14 +20,18 @@ func get_velocity(player: Player, delta: float) -> Vector3:
 		dash_velocity = _get_impulse_velocity(player.camera)
 		_stop_dash()
 	else:
-		dash_velocity = _calculate_dash_velocity(delta)
+		dash_velocity = _calculate_dash_velocity(player, delta)
 	return player.velocity + dash_velocity
 
 func _get_impulse_velocity(camera: Camera3D) -> Vector3:
 	return camera.global_transform.basis * Vector3(0, 0, DASH_SPEED * -1)
 
-func _calculate_dash_velocity(delta: float) -> Vector3:
-	return dash_velocity.move_toward(Vector3.ZERO, DASH_SPEED * delta)
+func _calculate_dash_velocity(player: Player, delta: float) -> Vector3:
+	var speed: float = DASH_SPEED
+	if player.is_on_floor():
+		speed = DASH_SPEED + FRICTION_FORCE
+	
+	return dash_velocity.move_toward(Vector3.ZERO, speed * delta)
 
 func _start_cooldown():
 	can_dash = false
