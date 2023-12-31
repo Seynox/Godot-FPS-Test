76ec2dc53b7fde15e357fa35af3b7ee1024eea63
Seynox <seynoxthegentleman@gmail.com>
2023-12-31 15:46:34 +0100

    Cleaned up simple jump code

--- abilities/jumps/simple_jump.gd
+++ abilities/jumps/simple_jump.gd
@@ -1,22 +1,19 @@
 class_name SimpleJump extends Jump
 
-var can_reset: bool = true # TODO Find a better way to avoid canceling jump on first tick
-
 func _execute(player: Player, _delta: float):
 	is_jumping = true
 	jump_velocity = _get_jump_velocity()
 	player.gravity_velocity = Vector3.ZERO
-	can_reset = false
 
 func _on_player_physics(player: Player, delta: float):
 	player.velocity += jump_velocity
+	var finished_jump: bool = player.is_on_floor() and player.velocity.y <= 0.0
 	
-	if player.is_on_floor() and can_reset:
+	if is_jumping and finished_jump:
 		is_jumping = false
 		jump_velocity = Vector3.ZERO
 		reload()
 	else:
-		can_reset = true
 		var gravity_force: float = player.GRAVITY * player.GRAVITY_MULTIPLIER
 		jump_velocity = jump_velocity.move_toward(Vector3.ZERO, gravity_force * delta)
 
