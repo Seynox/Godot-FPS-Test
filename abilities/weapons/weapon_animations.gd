7a8670fe3bb737cc76df273d9bb85291a1826e14
Seynox <seynoxthegentleman@gmail.com>
2024-01-04 22:33:24 +0100

    Fixed revolver animation-cancel

--- abilities/weapons/weapon_animations.gd
+++ abilities/weapons/weapon_animations.gd
@@ -7,9 +7,9 @@ extends AnimationPlayer
 func _on_uses_updated(previous_uses: int, uses_remaining: int):
 	var difference: int = previous_uses - uses_remaining
 	if difference > 0: # If the number of uses decreased
-		stop()
-		play(ATTACK_ANIMATION_NAME)
+		play("RESET")
+		queue(ATTACK_ANIMATION_NAME)
 
 func _on_reload():
-	stop()
-	play(RELOADING_ANIMATION_NAME)
+	play("RESET")
+	queue(RELOADING_ANIMATION_NAME)
